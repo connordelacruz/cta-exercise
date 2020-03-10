87e1329ff46f28af97de580179eae7d0c13c3146
Connor de la Cruz <connor.c.delacruz@gmail.com>
2020-03-10 12:37:29 -0500

    Added initial CSV file parsing
    Currently the file is parsed and a dump of the python list is returned.
    The data will need to be sorted and filtered, and a template will be
    used to render the data as a table.

--- cta/routes.py
+++ cta/routes.py
@@ -1,3 +1,4 @@
+import csv
 from flask import render_template
 from cta import app
 from cta.forms import CSVForm
@@ -8,8 +9,12 @@ def index():
     form = CSVForm()
     # If POST data is received and validated, parse CSV and render results
     if form.validate_on_submit():
-        # TODO
-        return 'Nice!'
+        file_string = form.csv_file.data.read().decode('utf-8')
+        reader = csv.DictReader(file_string.splitlines())
+        fields = reader.fieldnames
+        data = [row for row in reader]
+        # TODO: render template, pass data in context
+        return str(data)
     # Render index.html on GET request
     return render_template('index.html', title='Upload File', form=form)
 
